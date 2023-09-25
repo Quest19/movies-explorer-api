e9dc2daa9dc21606e2e7ec271dcf539990c09f58
Renat Khuzin <79320329+Quest19@users.noreply.github.com>
2023-09-25 03:53:26 +0300

    final poject 1.2 first iteration

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const { celebrate, Joi, errors } = require('celebrate');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cors = require('cors');
 const { createUser, login } = require('./controllers/users');
 const routerUser = require('./routes/users');
 const routerMovie = require('./routes/movies');
@@ -16,6 +18,8 @@ const { PORT = 4000 } = process.env;
 const app = express();
 
 mongoose.connect(DB_ADDRESS);
+
+app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
 
 app.use(bodyParser.json());
 
